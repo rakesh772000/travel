b2cb5900de356da658769131cadfa355c18618a2
rakesh <rakeshperam2@gmail.com>
2026-04-03 14:16:32 +0530

    added getbyname

--- src/main/java/com/travel/place_service/repository/PlaceRepository.java
+++ src/main/java/com/travel/place_service/repository/PlaceRepository.java
@@ -1,10 +1,13 @@
 package com.travel.place_service.repository;
 
+import com.travel.place_service.dto.PlaceResponse;
 import com.travel.place_service.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlaceRepository extends JpaRepository<Place,Long> {
-
+        List<Place> findByNameContainingIgnoreCase(String name);
 }
